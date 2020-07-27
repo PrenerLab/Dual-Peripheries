912ee921a691a7a94519d1f1b74714a923ff6039
Christopher Prener <chris.prener@slu.edu>
2020-07-27 16:08:51 -0400

    test query code

--- source/query_data.R
+++ source/query_data.R
@@ -1,24 +1,14 @@
 
-query_data <- function(category, year){
-  
-  ## establish connection
-  con <- DBI::dbConnect(RSQLite::SQLite(), "data/STL_DEMOGRAPHY_TractPop/data/STL_CITY_COUNTY_Database.sqlite")
-  
-  ## identify table
-  if (category == "population"){
-    db <- dplyr::tbl(con, "population")
-  } else if (category == "race"){
-    db <- dplyr::tbl(con, "race")
-  }
-  
-  ## query
-  out <- dplyr::filter(db, year == year)
-  out <- dplyr::collect(out)
-  
-  ## disconnect from database
-  DBI::dbDisconnect(con)
-  
-  ## return output
-  return(out)
-  
-}
+# dependencies ####
+
+library(dplyr)
+library(DBI)
+
+# load data ####
+## establish connection
+con <- dbConnect(RSQLite::SQLite(), "data/STL_DEMOGRAPHY_TractPop/data/STL_CITY_COUNTY_Database.sqlite")
+
+## collection records
+tbl(con, "population") %>%
+  filter(year == "1940") %>%
+  collect() -> pop40
